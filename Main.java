6576b3db2f45576e4d203b689fab525e8c2acc92
Jess Martin <jessmartin@gmail.com>
2021-04-07 01:57:10 +0000

    Filled out a bunch of things

--- Main.java
+++ Main.java
@@ -4,56 +4,75 @@ import java.net.ServerSocket;
 
 class Main {
   public static void main(String[] args) throws Exception {
-    // What's this "Socket" nonsense? A Socket is basically a "walkie-talkie"
-    // between two parties.
-    // You can think of it like a cable connecting two ports
+    // Start receiving messages - ready to receive messages!
     try (ServerSocket serverSocket = new ServerSocket(8080)) {
-      // Print "Server is running!" We're now accepting incoming messages.
-      System.out.println("Server started.\nListening for connections.");
+      System.out.println("Server started.\nListening for messages.");
 
-      // Infinite loop - usually these are bad! When is true not true? Never!
-      // We'll Run and Stop the program using repl.it's built-in feature for
-      // starting/stopping
-      // the server.
-      // Without this, we could only handle a single request and then the program
-      // would exit...
-      while (true) { // Just keep listening forever...
+      while (true) {
+        // Handle a new incoming message
 
-        // This method blocks waiting on an incoming connection.
-        // Think of it like sitting and waiting for a phone call to come in.
         try (Socket client = serverSocket.accept()) {
-          // System.out.println("Debug: got new client ");
-          System.out.println("Debug: got new client " + client.toString());
+          // client <-- messages queued up in it!!
+          System.out.println("Debug: got new message " + client.toString());
 
-          // Reads bytes and decodes them into characters using a specified charset
+          // Read the request - listen to the message
           InputStreamReader isr = new InputStreamReader(client.getInputStream());
 
-          // Reads text and buffers for efficient reading
           BufferedReader br = new BufferedReader(isr);
 
-          // Create a place to store our string
-          StringBuilder requestBuilder = new StringBuilder();
-
-          String line;
-          while (!(line = br.readLine()).isBlank()) {
-            requestBuilder.append(line + "\r\n");
+          // Read the first requeset from the client
+          StringBuilder request = new StringBuilder();
+          String line; // Temp variable called line that holds one line at a time of our message
+          line = br.readLine();
+          while (!line.isBlank()) {
+            request.append(line + "\r\n");
+            line = br.readLine();
           }
-
-          String request = requestBuilder.toString();
-          // System.out.println("--REQUEST--");
           // System.out.println(request);
 
-          OutputStream clientOutput = client.getOutputStream();
-          clientOutput.write(("HTTP/1.1 200 OK\r\n").getBytes());
-          clientOutput.write(("ContentType: text/plain \r\n").getBytes());
-          clientOutput.write("\r\n".getBytes());
-          clientOutput.write("Testing".getBytes());
-          clientOutput.flush();
-          client.close();
+          // Decide how we'd like to respond
 
-          // System.out.println("sent response");
-          // At some point, we might want to refactor and extract a method
-          // handleConnection(client);
+            // Get the first line of the request
+            String firstLine = request.toString().split("\n")[0];
+            // Get the second thing "resource" from the first line (separated by spaces)
+            String resource = firstLine.split(" ")[1];
+            // Compare the "resource" to our list of things
+            System.out.println(resource);
+            if (resource.equals("/jess")) {
+              System.out.println(resource.equals("/jess"));
+              FileInputStream image = new FileInputStream​("fav.jpg");
+              OutputStream clientOutput = client.getOutputStream();
+              clientOutput.write(("HTTP/1.1 200 OK\r\n").getBytes());
+              clientOutput.write(("\r\n").getBytes());
+              clientOutput.write(image.readAllBytes());
+              clientOutput.flush();
+            } else if (resource.equals("/hello")) {
+              OutputStream clientOutput = client.getOutputStream();
+              clientOutput.write(("HTTP/1.1 200 OK\r\n").getBytes());
+              clientOutput.write(("\r\n").getBytes());
+              clientOutput.write(("Hello World").getBytes());
+              clientOutput.flush();
+            } else {
+              OutputStream clientOutput = client.getOutputStream();
+              clientOutput.write(("HTTP/1.1 200 OK\r\n").getBytes());
+              clientOutput.write(("\r\n").getBytes());
+              clientOutput.write(("What ya lookin' for?").getBytes());
+              clientOutput.flush();
+            }
+            // Send back the appropriate thing based on resource
+
+            // Just send back a simple "Hello World"
+
+            // Send back an image
+
+            // Load the image from the filesystem
+
+            // Change response based on route?
+
+          // Send a response - send our reply
+
+          // Get ready for the next message
+          client.close();
         }
       }
     }
